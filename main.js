a2481e4b7ae5e570c397abdafe5e94bcda9d2e76
Nick <morrinic@oregonstate.edu>
2019-11-18 15:40:57 -0800

    Exercise 15 Writing Functions with Javascript
    h2 updates to ride/count

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
+const BASE_URL = "https://zagster-service.herokuapp.com"
+
 $(updateView)
 
 function updateView() {
     alert("Data Visualization Project")
 }
 
+function updateView() {
+    $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+  }
+  
+  function updateRideCount(data) {
+    numberOfRides = data.count
+    $("h2#rideCount").html(numberOfRides)
+  }
 
   google.charts.load('current', {'packages':['corechart']});
 
   google.charts.setOnLoadCallback(drawwinterchart);
-  google.charts.setOnLoadCallback(drawspringchart);
   google.charts.setOnLoadCallback(drawsummerchart);
+  google.charts.setOnLoadCallback(drawfallchart);
+  google.charts.setOnLoadCallback(drawspringchart);
 
   function drawwinterchart() {
 
@@ -23,7 +34,7 @@ function updateView() {
     ]);
 
 
-    var options = {title:'Total Rides in Winter',
+    var options = {title:'Total Rides from November to January',
                    width:400,
                    height:300};
 
@@ -32,6 +43,48 @@ function updateView() {
     chart.draw(data, options);
   }
 
+
+  function drawsummerchart() {
+
+
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Memberships');
+    data.addColumn('number', 'Total');
+    data.addRows([
+      ['Students', 4],
+      ['Others', 2],
+    ]);
+
+
+    var options = {title:'Total Rides from June to August',
+                   width:400,
+                   height:300};
+
+
+    var chart = new google.visualization.PieChart(document.getElementById('Summerdiv'));
+    chart.draw(data, options);
+  }
+
+  function drawfallchart() {
+
+
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Memberships');
+    data.addColumn('number', 'Total');
+    data.addRows([
+      ['Students', 4],
+      ['Others', 2],
+    ]);
+
+
+    var options = {title:'Total Rides from June to August',
+                   width:400,
+                   height:300};
+
+
+    var chart = new google.visualization.PieChart(document.getElementById('Falldiv'));
+    chart.draw(data, options);
+  }
 
   function drawspringchart() {
 
@@ -45,32 +98,11 @@ function updateView() {
     ]);
 
 
-    var options = {title:'Total Rides in Spring',
+    var options = {title:'Total Rides from June to August',
                    width:400,
                    height:300};
 
 
     var chart = new google.visualization.PieChart(document.getElementById('Springdiv'));
-    chart.draw(data, options);
-  }
-
-  function drawsummerchart() {
-
-
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Memberships');
-    data.addColumn('number', 'Total');
-    data.addRows([
-      ['Students', 8],
-      ['Others', 5],
-    ]);
-
-
-    var options = {title:'Total Rides in Summer',
-                   width:400,
-                   height:300};
-
-
-    var chart = new google.visualization.PieChart(document.getElementById('Summerdiv'));
     chart.draw(data, options);
   }
